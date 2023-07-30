95f9258b9960ad80e1551fe04a1e4a0e782a28b2
quasemago <brunoronningfn@gmail.com>
2023-07-30 17:26:05 -0400

    Add check for owner in stop command

--- src/main/java/dev/quasemago/maidbot/commands/Stop.java
+++ src/main/java/dev/quasemago/maidbot/commands/Stop.java
@@ -2,7 +2,9 @@ package dev.quasemago.maidbot.commands;
 
 import dev.quasemago.maidbot.MaidBotApplication;
 import dev.quasemago.maidbot.core.SlashCommand;
+import dev.quasemago.maidbot.helpers.Utils;
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent;
+import discord4j.core.object.entity.User;
 import discord4j.core.object.entity.channel.MessageChannel;
 import discord4j.core.object.entity.channel.PrivateChannel;
 import discord4j.rest.util.Permission;
@@ -11,7 +13,6 @@ import reactor.core.publisher.Mono;
 
 @Component
 public class Stop extends SlashCommand<ChatInputInteractionEvent> {
-    // TODO: Create custom permission system to allow only owner.
     @Override
     public Mono<Void> exe(ChatInputInteractionEvent event) {
         final MessageChannel channel = event.getInteraction()
@@ -19,6 +20,16 @@ public class Stop extends SlashCommand<ChatInputInteractionEvent> {
                 .block();
 
         if (!(channel instanceof PrivateChannel)) {
+            final User author = event.getInteraction()
+                    .getUser();
+
+            // TODO: Create a custom permissions system to only allow commands for the owner,
+            //  instead of hardcoded this in the command event.
+            if (!Utils.isBotOwner(author)) {
+                return event.reply("Only the bot owner can use this command.")
+                        .withEphemeral(true);
+            }
+
             return event.reply()
                     .withEphemeral(false)
                     .withContent("Bot is stopping...")
