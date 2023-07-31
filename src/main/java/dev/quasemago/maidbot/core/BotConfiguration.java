48e0e5e4fdbc84b3877edb15974dca2e26994eae
quasemago <brunoronningfn@gmail.com>
2023-07-31 00:29:07 -0400

    Create memes command Add mysql database and memes dependencies (models, repository, etc)

--- src/main/java/dev/quasemago/maidbot/core/BotConfiguration.java
+++ src/main/java/dev/quasemago/maidbot/core/BotConfiguration.java
@@ -21,7 +21,7 @@ import java.util.List;
 
 @Configuration
 public class BotConfiguration {
-    private static Dotenv dotenv;
+    private static final Dotenv dotenv = Dotenv.load();
 
     public String getBotToken() {
         return dotenv.get("BOT_TOKEN");
@@ -32,10 +32,12 @@ public class BotConfiguration {
     public static Snowflake getBotOwner() {
         return Snowflake.of(dotenv.get("BOT_OWNER"));
     }
+    public static String getBotDatabaseUrl() {
+        return dotenv.get("BOT_DATABASE_URL");
+    }
 
     @Bean
     public <T extends Event>GatewayDiscordClient gatewayDiscordClient(final List<EventListener<T>> eventListenerList) {
-        dotenv = Dotenv.load();
         if (this.getBotToken().isEmpty()) {
             Logger.log.fatal("Bot token is empty, please set BOT_TOKEN environment variable!");
             System.exit(0);
---
+++ src/main/java/dev/quasemago/maidbot/models/MemesModel.java
@@ -0,0 +1,87 @@
+package dev.quasemago.maidbot.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.sql.Date;
+
+@Entity
+@Table(name = "memes")
+public class MemesModel implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false, unique = true)
+    private Long id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String url;
+    @Column(name = "date_created", nullable = false)
+    private Date dateCreated;
+    @Column(name = "date_updated", nullable = false)
+    private Date dateUpdated;
+    @Column(name = "guild_id", nullable = false)
+    private Long guildId;
+
+    public MemesModel() {}
+
+    public MemesModel(Date date_created, Date date_updated, Long guild_id, String name, String url) {
+        this.dateCreated = date_created;
+        this.dateUpdated = date_updated;
+        this.guildId = guild_id;
+        this.name = name;
+        this.url = url;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date date_created) {
+        this.dateCreated = date_created;
+    }
+
+    public Date getDateUpdated() {
+        return dateUpdated;
+    }
+
+    public void setDateUpdated(Date date_updated) {
+        this.dateUpdated = date_updated;
+    }
+
+    public Long getGuildId() {
+        return guildId;
+    }
+
+    public void setGuildId(Long guild_id) {
+        this.guildId = guild_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
