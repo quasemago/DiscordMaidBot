ce96c92003da873610982f49a6d3aaaba5017d10
quasemago <brunoronningfn@gmail.com>
2023-07-31 16:25:17 -0400

    Bump release version

--- src/main/java/dev/quasemago/maidbot/MaidBotApplication.java
+++ src/main/java/dev/quasemago/maidbot/MaidBotApplication.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @SpringBootApplication
 @EnableScheduling
 public class MaidBotApplication {
-	public static final String VERSION = "0.0.3";
+	public static final String VERSION = "0.0.4";
 	public static final String GITHUB_URL = "https://github.com/quasemago/DiscordMaidBot";
 	public static final String GITHUB_AUTHOR = "quasemago";
 	public static final String GITHUB_AUTHOR_URL = "https://github.com/quasemago";
